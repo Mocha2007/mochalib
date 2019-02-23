d236406da7ced52c793b6488e780f9b0dbbfcd3a
Mocha <mocha2007andcoco@gmail.com>
2019-02-23 12:51:32 -0500

    VERY WIP

---
+++ moki_interpreter.py
@@ -0,0 +1,307 @@
+from math import ceil, log
+from random import choice, random
+from time import time
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+
+def mode(x):
+	"""Return the mode of an iterable"""
+	return max(set(x), key=x.count)
+
+
+def alphabet_generator(x: str) -> str:
+	assert len(x) == 1
+	if x in alphabet.upper():
+		return alphabet[:alphabet.index(x.lower())+1].upper()
+	return alphabet[:alphabet.index(x)+1]
+
+
+def random_program(n: int) -> str:
+	valid_chars = list(type_specific_behavior.keys()) + list(functions.keys()) + list("#':;\\`")
+	return ''.join(choice(valid_chars) for _ in range(n))
+
+
+def error(**kwargs):
+	kwargs['s'] = stack
+	pointer = ' '*kwargs['i'] + '^'
+	kwargs['p'] = pointer
+	kwargs['b'] = stack_history[-1]
+	print('{x}\n{p}\nchar {i} {c}: {e}\ns: {b} -> {s}\nf: {f}'.format(**kwargs))
+
+
+type_specific_behavior = {
+	'$': {
+		len: 1,
+		(int,): lambda x: stack[-x],
+		(list,): sorted,
+		(str,): sorted,
+	},
+	'"': {
+		len: 1,
+		(int,): chr,
+		(list,): lambda x: ''.join([chr(i) for i in x]),
+		(str,): lambda x: [ord(i) for i in x],
+	},
+	'(': {
+		len: 1,
+		(float,): lambda x: x-1,
+		(int,): lambda x: x-1,
+		(list,): lambda x: [i-1 for i in x],
+		(str,): lambda x: ''.join([chr(ord(i)-1) for i in x]),
+	},
+	')': {
+		len: 1,
+		(float,): lambda x: x+1,
+		(int,): lambda x: x+1,
+		(list,): lambda x: [i+1 for i in x],
+		(str,): lambda x: ''.join([chr(ord(i)+1) for i in x]),
+	},
+	'-': {
+		len: 2,
+		(float, float): lambda a, b: a-b,
+		(float, int): lambda a, b: a-b,
+		(int, int): lambda a, b: a-b,
+		(int, float): lambda a, b: a-b,
+		(list, float): lambda a, b: list(filter(lambda x: x != b, a)),
+		(list, int): lambda a, b: list(filter(lambda x: x != b, a)),
+		(list, list): lambda a, b: list(filter(lambda x: x not in b, a)),
+		(list, str): lambda a, b: list(filter(lambda x: x != b, a)),
+		(str, int): lambda a, b: a[:-b],
+		(str, str): lambda a, b: ''.join(list(filter(lambda x: x not in b, list(a)))),
+	},
+	'/': {
+		len: 2,
+		(float, float): lambda a, b: a/b,
+		(float, int): lambda a, b: a/b,
+		(int, int): lambda a, b: a/b,
+		(int, float): lambda a, b: a/b,
+		(list, float): lambda a, b: a[:len(a)//b],
+		(list, int): lambda a, b: a[:len(a)//b],
+		(list, list): lambda a, b: a.count(b),
+		(list, str): lambda a, b: a.count(b),
+		(str, float): lambda a, b: a[:len(a)//b],
+		(str, int): lambda a, b: a[:len(a)//b],
+		(str, str): lambda a, b: a.count(b),
+	},
+	'M': {
+		len: 0,
+		tuple(): lambda: None if empty_stack() else sum(stack)/len(stack),
+	},
+	'[': {
+		len: 1,
+		(float,): int,
+		(int,): int,
+		(list,): min,
+		(str,): lambda x: x.lower(),
+	},
+	']': {
+		len: 1,
+		(float,): ceil,
+		(int,): int,
+		(list,): max,
+		(str,): lambda x: x.upper(),
+	},
+	'm': {
+		len: 0,
+		tuple(): lambda: None if empty_stack() else mode(stack),
+	},
+	'n': {
+		len: 1,
+		(int,): lambda x: -x,
+		(str,): alphabet_generator,
+	},
+	'|': {
+		len: 1,
+		(float,): abs,
+		(int,): abs,
+		(list,): len,
+		(str,): len,
+	},
+	'~': {
+		len: 1,
+		(float,): lambda x: -x,
+		(int,): lambda x: -x,
+		(list,): lambda x: x[::-1],
+		(str,): lambda x: x[::-1],
+	},
+}
+
+
+# special functions
+def type_specific_function(char: str):
+	args = tuple(stack.pop() for _ in range(type_specific_behavior[char][len]))
+	return type_specific_behavior[char][tuple(type(arg) for arg in args)](*args)
+
+
+def empty_stack():
+	global stack
+	stack = []
+
+
+# functions
+functions = {
+	'!': lambda: int(not stack.pop()),
+	# " (USED) chr/ord
+	# # (USED) comments
+	# $ (USED) int -> stack item at index; list/string -> sort
+	'%': lambda: stack.pop() % stack.pop(),
+	'&': lambda: stack.pop() & stack.pop(),
+	# ' (USED) string creation
+	# ( (USED) decrement
+	# ) (USED) increment
+	'*': lambda: stack.pop() * stack.pop(),
+	'+': lambda: stack.pop() + stack.pop(),
+	',': lambda: 0,
+	# - (USED) numeric/set subtraction
+	'.': lambda: stack[-1],
+	# / (USED) numeric division, special behavior with lists/strings
+	'0': lambda: stack.pop()*10,
+	'1': lambda: stack.pop()*10+1,
+	'2': lambda: stack.pop()*10+2,
+	'3': lambda: stack.pop()*10+3,
+	'4': lambda: stack.pop()*10+4,
+	'5': lambda: stack.pop()*10+5,
+	'6': lambda: stack.pop()*10+6,
+	'7': lambda: stack.pop()*10+7,
+	'8': lambda: stack.pop()*10+8,
+	'9': lambda: stack.pop()*10+9,
+	# : (USED) assignment
+	# ; (USED) pop
+	'<': lambda: int(stack.pop() < stack.pop()),
+	'=': lambda: int(stack.pop() == stack.pop()),
+	'>': lambda: int(stack.pop() > stack.pop()),
+	'?': lambda: (lambda *x: x[1] if x[0] else x[2])(stack.pop(), stack.pop(), stack.pop()),
+	'@': lambda: stack.pop(0),
+	# M (USED) mean
+	# [ (USED) floor/lowercase/min
+	# \ (USED) swap top two
+	# ] (USED) ceiling/uppercase/max
+	'^': lambda: stack.pop()**stack.pop(),
+	# _
+	# ` (USED) function call
+	'a': lambda: [],
+	'l': lambda: log(stack.pop()),
+	# m (USED) mode
+	# n (USED) 5 n -> [1, 2, 3, 4, 5]; 'c' n -> 'abc'
+	# 'p': lambda: [stack.pop()] + stack.pop(),
+	'r': lambda: random(),
+	't': lambda: time(),
+	'z': lambda: list(zip(stack.pop())),
+	# {
+	# | (USED) abs/len
+	# }
+	# ~ (USED) negation/reversal
+}
+
+
+# execute line
+def run(program: str, **kwargs):
+	if 'declared_functions' in kwargs:
+		declared_functions = kwargs['declared_functions']
+	else:
+		declared_functions = {}
+	is_comment = False
+	is_string = False
+	is_escape = False
+	for i, char in enumerate(program):
+		stack_history.append(list(stack))
+		if is_string:
+			# don't add strings to ints n shit
+			if type(stack[-1]) != str:
+				stack.append('')
+			# escape chars
+			if is_escape:
+				is_escape = False
+				stack[-1] += char
+			elif char == '\\':
+				is_escape = True
+			elif char == '\'':
+				is_string = False
+			else:
+				# otherwise, concat
+				stack[-1] += char
+			continue
+		try:
+			# SPECIAL FUNCTIONS THAT MUST BE RUN BEFORE
+			# comments
+			if char == '#':
+				is_comment = not is_comment
+			# strings
+			elif char == '\'':
+				is_string = True
+			# function assignment
+			elif char == ':':
+				a, b = stack.pop(), stack.pop()
+				declared_functions[a] = str(b)
+			# stack pop
+			elif char == ';':
+				stack.pop()
+			# top two in stack swap
+			elif char == '\\':
+				a, b = stack.pop(), stack.pop()
+				stack.append(b)
+				stack.append(a)
+			# function execution
+			elif char == '`':
+				run(declared_functions[stack.pop()], declared_functions=declared_functions)
+			# really special
+			elif char in type_specific_behavior:
+				stack.append(type_specific_function(char))
+			# nop
+			if char not in functions:
+				continue
+			# main code
+			stack.append(functions[char]())
+		except Exception as this_exception:
+			error(c=char, e=this_exception, f=declared_functions, i=i, x=program)
+			return None
+
+
+class Stack:
+	count = 0
+
+	def __init__(self, n=1):
+		self.list = [0]*n
+
+	def __getitem__(self, item):
+		return self.list[item]
+
+	def __iter__(self):
+		for i in self.list:
+			yield i
+
+	def __len__(self) -> int:
+		return len(self.list)
+
+	def __repr__(self) -> str:
+		return str(self.list).replace(',', '')
+
+	def __setitem__(self, key, value):
+		self.__dict__[key] = value
+
+	def append(self, x):
+		self.push(x)
+
+	def pop(self, *args):
+		try:
+			return self.list.pop(*args)
+		except IndexError:
+			return 0
+
+	def push(self, x):
+		self.list.append(x)
+
+
+# main
+while 1:
+	stack = Stack() # [0] <> Stack()
+	stack_history = []
+	code = input('>>> ')
+	code = random_program(20)
+	print(code)
+	try:
+		run(code)
+		print(stack)
+	except Exception as e:
+		print(e)
