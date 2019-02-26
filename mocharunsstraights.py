f061b09750886df32f17517b0bd010d6066b869d
Mocha <mocha2007andcoco@gmail.com>
2019-02-25 19:45:49 -0500

    mocharunsstraights cleanup

--- mocharunsstraights.py
+++ mocharunsstraights.py
@@ -1,33 +1,34 @@
 from collections import Counter
-from itertools import groupby
-def straights(list):
-	tot=[]
-	count=Counter(list)
-	for i in set(list):
-		tot+=[[i]*count[i]]
+
+
+def straights(lis: list) -> list:
+	tot = []
+	count = Counter(lis)
+	for i in set(lis):
+		tot.append([i]*count[i])
 	tot.sort()
 	return tot
-			
-def runs(list):
-	tot=[]
-	for i in list:
-		run=[i]
-		size=1
-		while 1:
-			if i+size in list:
-				run+=[i+size]
-				size+=1
-			else:break
-		tot+=[run]
+
+
+def runs(lis: list) -> list:
+	tot = []
+	for i in lis:
+		run = [i]
+		size = 1
+		while i+size in lis:
+			run.append(i+size)
+			size += 1
+		tot.append(run)
 	tot.sort()
 	return [tot[i] for i in range(len(tot)) if i == 0 or tot[i] != tot[i-1]]
 
-def sor(list):
-	max=[]
-	for i in runs(list):
-		if len(i)>len(max):
-			max=i
-	for i in straights(list):
-		if len(i)>len(max):
-			max=i
-	return max
+
+def sor(lis: list) -> int:
+	m = []
+	for i in runs(lis):
+		if len(m) < len(i):
+			m = i
+	for i in straights(lis):
+		if len(m) < len(i):
+			m = i
+	return m
