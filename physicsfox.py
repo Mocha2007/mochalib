84c2b319f701275ee7ad407ceda29caa0cc48e7f
Mocha <mocha2007andcoco@gmail.com>
2019-02-28 16:41:31 -0500

    dimension class + fixes

--- physicsfox.py
+++ physicsfox.py
@@ -18,8 +18,39 @@ def add_dims(a: Dict[str, int], b: Dict[str, int]) -> Dict[str, int]:
 	return new_dict
 
 
+class Dimension:
+	def __init__(self, **dimensions):
+		self.dimensions = dimensions # type: Dict[str, int]
+
+	def __repr__(self) -> str:
+		dimensions = [key.upper()+'^'+str(value) for key, value in sorted(self.dimensions.items(), key=lambda x: x[0])]
+		return ' '.join(dimensions)
+
+	def __bool__(self) -> bool:
+		return bool(self.dimensions)
+
+	def __invert__(self):
+		return Dimension(**{key: -value for key, value in self.dimensions.items()})
+
+	def __mul__(self, other):
+		a, b = self.dimensions, other.dimensions
+		new_dict = {}
+		for key in set(a) | set(b):
+			new_dict[key] = 0
+			if key in a:
+				new_dict[key] += a[key]
+			if key in b:
+				new_dict[key] += b[key]
+			if new_dict[key] == 0:
+				del new_dict[key]
+		return Dimension(**new_dict)
+
+	def __truediv__(self, other):
+		return self * ~other
+
+
 class Unit:
-	def __init__(self, symbol: str = '', si_multiple: float = 1, dimension: Dict[str, int] = None):
+	def __init__(self, symbol: str = '', si_multiple: float = 1, dimension: Dimension = Dimension(**{})):
 		self.symbol = symbol
 		self.si_multiple = si_multiple
 		self.dimension = dimension
@@ -27,14 +58,21 @@ class Unit:
 	def __repr__(self) -> str:
 		return self.symbol
 
+	def __invert__(self):
+		return Unit('/'+self.symbol, 1/self.si_multiple, ~self.dimension)
+
 	def __mul__(self, other):
-		dimension = add_dims(self.dimension, other.dimension)
-		if dimension:
-			return Unit(self.symbol+other.symbol, self.si_multiple*other.si_multiple, dimension)
-		return self.si_multiple*other.si_multiple
+		dimension = self.dimension * other.dimension
+		# if dimension:
+		return Unit(self.symbol+other.symbol, self.si_multiple*other.si_multiple, dimension)
+		# return self.si_multiple*other.si_multiple
 
 	def __truediv__(self, other):
-		return self * Unit('/'+other.symbol, other.si_multiple, {key: -val for key, val in other.dimension.items()})
+		return self * ~other
+
+	def __floordiv__(self, other):
+		assert self.dimension == other.dimension
+		return (self/other).si_multiple
 
 	def __pow__(self, power: int):
 		assert isinstance(power, int)
@@ -66,13 +104,21 @@ class Value:
 		multiple = 10**(-3*index)
 		return Value(self.value*multiple, Unit(prefix+self.unit.symbol), self.uncertainty*multiple).__repr__()
 
+	def convert(self, desired_unit: Unit):
+		assert self.unit.dimension == desired_unit.dimension
+		scalar = self.unit // desired_unit
+		return Value(self.value*scalar, desired_unit, self.uncertainty*scalar)
+
+	def __neg__(self):
+		return Value(-self.value, self.unit, self.uncertainty)
+
 	def __add__(self, other):
 		assert self.unit.dimension == other.unit.dimension
-		return Value(self.value+other.value, self.unit, self.uncertainty+other.uncertainty)
+		scalar = other.unit // self.unit
+		return Value(self.value+other.value*scalar, self.unit, self.uncertainty+other.uncertainty)
 
 	def __sub__(self, other):
-		assert self.unit.dimension == other.unit.dimension
-		return Value(self.value-other.value, self.unit, self.uncertainty+other.uncertainty)
+		return self + -other
 
 	def __mul__(self, other):
 		uncertainty = self.value * other.uncertainty + other.value * self.uncertainty
@@ -89,16 +135,21 @@ class Value:
 	# 	return Value(self.value**power, self.unit**power, uncertainty/2)
 
 
-kilogram = Unit('kg', 1, {'kg': 1})
-meter = Unit('m', 1, {'m': 1})
-second = Unit('s', 1, {'s': 1})
+mass = Dimension(m=1)
+length = Dimension(l=1)
+time = Dimension(t=1)
+
+
+kilogram = Unit('kg', 1, mass)
+meter = Unit('m', 1, length)
+second = Unit('s', 1, time)
 newton = kilogram * meter / second / second
 newton.symbol = 'N'
 joule = newton * meter
 joule.symbol = 'J'
 watt = joule / second
 watt.symbol = 'W'
-foot = Unit('ft', 0.3048, {'m': 1})
+foot = Unit('ft', 0.3048, length)
 
 # tests
 # print(meter) # m
