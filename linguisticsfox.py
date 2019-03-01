ba3e8118c8be9bae4170eee3704dab789c0cf894
Mocha <mocha2007andcoco@gmail.com>
2019-03-01 13:31:28 -0500

    added morpheme class

--- linguisticsfox.py
+++ linguisticsfox.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Set, Tuple
+from typing import Dict, Set, Tuple
 from random import choice, random
 
 
@@ -15,6 +15,20 @@ class Morpheme:
 	def __init__(self, phonemes: Tuple[Phoneme] = tuple()):
 		self.phonemes = phonemes
 
+	def __repr__(self) -> str:
+		return ''.join([str(i) for i in self.phonemes])
+
+	def __getitem__(self, item) -> Phoneme:
+		return self.phonemes[item]
+
+	def __len__(self) -> int:
+		return len(self.phonemes)
+
+	def append(self, phoneme: Phoneme):
+		self.phonemes = list(self.phonemes)
+		self.phonemes.append(phoneme)
+		self.phonemes = tuple(self.phonemes)
+
 
 class Environment:
 	def __init__(self, before: Set[Phoneme], after: Set[Phoneme]):
@@ -29,30 +43,27 @@ class Environment:
 
 class Phonotactics:
 	def __init__(self, syllable_structure: Tuple[Tuple[Tuple[Set[Phoneme], bool]]], # bool is for "is this mandatory?"
-				constraints: Tuple[Tuple[Set[Phoneme], Environment, bool]], syllable_count: (int, int) = (1, 3)):
+				constraints: Tuple[Tuple[Set[Phoneme], Environment, bool]], syllable_count: (int, int) = (1, 4)):
 		self.syllable_strcuture = syllable_structure
 		self.constraints = constraints
 		self.syllable_count = syllable_count
 
-	def generate_morpheme(self) -> Tuple[Phoneme]:
-		morph = [] # type: List[Phoneme]
+	def generate_morpheme(self) -> Morpheme:
+		morph = Morpheme()
 		for _ in range(*self.syllable_count):
 			syllable_template = choice(self.syllable_strcuture)
-			# syllable = []
 			for phoneme_set, b in syllable_template:
-				if b or random():
+				if b or .5 < random():
 					morph.append(choice(tuple(phoneme_set)))
-			# morph.append(tuple(syllable))
-		morph = tuple(morph) # type: Tuple[Phoneme]
 		return morph if self.obeys(morph) else self.generate_morpheme()
 
-	def obeys(self, word: Tuple[Phoneme]) -> bool:
-		for i, phoneme in enumerate(word):
+	def obeys(self, morph: Morpheme) -> bool:
+		for i, phoneme in enumerate(morph):
 			for phoneme_set, environment, b in self.constraints:
 				if phoneme not in phoneme_set:
 					continue
-				before = word[i-1] if i-1 else None
-				after = word[i+1] if i+1 < len(word) else None
+				before = morph[i-1] if i-1 else None
+				after = morph[i+1] if i+1 < len(morph) else None
 				if not (environment.obeys(before, after) == b):
 					return False
 		return True
@@ -60,7 +71,7 @@ class Phonotactics:
 
 phoneme_a = Phoneme('a', **{'open': True, 'front': True, 'rounded': False, 'vowel': True})
 phoneme_b = Phoneme('b', **{'voiced': True, 'bilabial': True, 'plosive': True, 'consonant': True})
-syllable_a = ({phoneme_b}, False), ({phoneme_a}, True)
+syllable_a = ({phoneme_b}, False), ({phoneme_a}, True) # type: Tuple[Tuple[Set[Phoneme], bool]]
 phonotactics_a = Phonotactics((syllable_a,), tuple())
 new_morpheme = phonotactics_a.generate_morpheme()
 print(new_morpheme)
