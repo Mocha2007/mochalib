662d6fb6a7ebe47a8ff55d654d9c55b61830dc37
Mocha <mocha2007andcoco@gmail.com>
2019-06-29 21:39:51 -0400

    length comparison, imperial units

--- mochaunits.py
+++ mochaunits.py
@@ -16,9 +16,33 @@ class Length:
 			return str(x/au) + ' au'
 		return str(x/ly) + ' ly'
 
+	@property
+	def imperial(self) -> str:
+		x = self.value
+		inch = 2.54e-2
+		ft = 0.3048
+		yd = 0.9144
+		mi = 1609.344
+		if self.value < ft:
+			return str(x/inch) + ' in'
+		if self.value < yd:
+			return str(x/ft) + ' ft'
+		if self.value < mi:
+			return str(x/yd) + ' yd'
+		return str(x/mi) + ' mi'
+
 	# double underscore methods
 	def __add__(self, other):
 		return Length(self.value+other.value)
+
+	def __eq__(self, other) -> bool:
+		return self.value == other.value
+
+	def __le__(self, other) -> bool:
+		return self.value <= other.value
+
+	def __lt__(self, other) -> bool:
+		return self.value < other.value
 
 	def __neg__(self):
 		return Length(-self.value)
@@ -30,6 +54,8 @@ class Length:
 		return 'Length({0})'.format(self.value)
 
 	def __str__(self) -> str:
+		if 'imperial' in self.tags:
+			return self.imperial
 		x = self.value
 		if x < 1e-6:
 			return str(x*1e9) + ' nm'
