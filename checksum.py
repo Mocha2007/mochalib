6a0c4382e716beef982cf453de9ff33038bbf976
Mocha <mocha2007andcoco@gmail.com>
2019-08-11 20:07:44 -0400

    certainty data

--- checksum.py
+++ checksum.py
@@ -1,7 +1,15 @@
 from textwrap import wrap
 
-def string_checksum(string: str, b: int=4) -> int:
-	"""outputs an 8b-bit integer (default: 32-bit)"""
+def string_checksum(string: str, b: int=2) -> int:
+	"""outputs an 8b-bit integer (default: 16-bit)
+	b	sigma
+	1	2.885    8-bit
+	2	4.324   16-bit
+	3	5.419   24-bit
+	4	6.337   32-bit
+	8	9.155   64-bit
+	16	13.108 128-bit
+	"""
 	string += '\0'*(len(string) % b) # so the string length is divisible by three
 	checksum = 0
 	for substring in wrap(string, b):
