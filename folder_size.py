4d62d57b1bd3c6d7f1c16f341eaf51c1ac296e1b
Mocha <mocha2007andcoco@gmail.com>
2019-09-23 20:56:55 -0400

    folder size tool

---
+++ folder_size.py
@@ -0,0 +1,30 @@
+import os
+from math import log10
+
+def get_size(start_path = '.'):
+	"""Get size of directory"""
+	total_size = 0
+	for dirpath, dirnames, filenames in os.walk(start_path):
+		for f in filenames:
+			fp = os.path.join(dirpath, f)
+			try:
+				total_size += os.path.getsize(fp)
+			except FileNotFoundError:
+				pass
+	return total_size
+
+def get_subdirectories():
+	return next(os.walk('.'))[1]
+
+def get_sizes_of_subdirectories():
+	subs = get_subdirectories()
+	return sorted([(name, get_size(name)) for name in subs], key=lambda x: x[1])
+
+def prettify_size(entry: (str, int)) -> str:
+	name, size = entry
+	prefixes = '', 'k', 'M', 'G', 'T'
+	level = int(log10(size)/3) if size else 0
+	return str(size//10**(3*level)) + ' {0}b'.format(prefixes[level]) + '\t' + name
+
+while 1:
+	input('\n'.join(map(prettify_size, get_sizes_of_subdirectories())))
