acb42e51a24d76b71e30ef75a9027400b9e561a2
Mocha <mocha2007andcoco@gmail.com>
2019-10-29 21:00:52 -0400

    better results with wilson score

--- hebbian.py
+++ hebbian.py
@@ -1,7 +1,7 @@
 from re import sub
 from typing import Dict, Set
 
-corpus_filename = 'books/kjb.txt'
+corpus_filename = 'books/star wars.txt'
 corpus_file = open(corpus_filename, 'r').read()
 
 
@@ -54,7 +54,8 @@ def main(text: str) -> Dict[str, Dict[str, int]]:
 	return mapping
 
 
-def pretty_associations(raw_corp: str, target: str, limit: int = -1):
+def pretty_associations(raw_corp: str, target: str, limit: int = 20):
+	"""-1 for no limit"""
 	clean_corpus = clean(raw_corp)
 	word_dict = main(raw_corp)[target] # type: Dict[str, int]
 	counts = {word: word_dict[word] / clean_corpus.count(word)**.7 for word in word_dict} # optimal exp somewhere between .5 and .8?
@@ -65,6 +66,29 @@ def pretty_associations(raw_corp: str, target: str, limit: int = -1):
 		print(word, '\t', r)
 
 
+def counts_to_wilson(word_dict_count: int, clean_corpus_count: int) -> float:
+	up = word_dict_count
+	n = clean_corpus_count
+	down = n - up
+	z = 1.96
+	p1 = (up+z**2/2)/(n+z**2)
+	p2 = z/(n+z**2)*(up*down/n+z**2/4)**.5
+	return p1 - p2
+
+
+def pretty_associations_wilson(raw_corp: str, target: str, limit: int = 20):
+	"""-1 for no limit"""
+	clean_corpus = clean(raw_corp)
+	word_dict = main(raw_corp)[target] # type: Dict[str, int]
+	counts = {word: counts_to_wilson(word_dict[word], clean_corpus.count(word)) for word in word_dict}
+	sorted_words = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+	for i, (word, r) in enumerate(sorted_words):
+		if 0 <= limit and limit < i:
+			break
+		print(word, '\t', r)
+
+
 # output = main(corpus_file)
-# pretty_associations(corpus_file, 'luke', 20)
-pretty_associations(corpus_file, 'luke', 20)
+# pretty_associations(corpus_file, 'luke')
+# print('\n')
+pretty_associations_wilson(corpus_file, 'luke')
