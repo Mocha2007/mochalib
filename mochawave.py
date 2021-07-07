5ff437ef28dd05848d26d8317be56fc59cf9f361
Mocha <3129210+Mocha2007@users.noreply.github.com>
2021-07-07 08:58:29 -0400

    gen square

--- mochawave.py
+++ mochawave.py
@@ -2,16 +2,16 @@ from math import pi, log2, sin
 from typing import Iterable
 
 # basic waveforms
-def square(x: float, freq: float, amp: float) -> float:
+def square(x: float, freq: float, amp: float = 1) -> float:
 	return amp*(2*(x/freq//1%2)-1)
 
-def triangle(x: float, freq: float, amp: float) -> float:
+def triangle(x: float, freq: float, amp: float = 1) -> float:
 	return amp*(abs(4*((x/freq)%1)-2)-1)
 
-def sine(x: float, freq: float, amp: float) -> float:
+def sine(x: float, freq: float, amp: float = 1) -> float:
 	return amp*sin(2*pi*x/freq)
 
-def sawtooth(x: float, freq: float, amp: float) -> float:
+def sawtooth(x: float, freq: float, amp: float = 1) -> float:
 	return amp*(2*((x/freq)%1)-1)
 
 # note to freq conversions
@@ -49,3 +49,13 @@ class Wave:
 		"""transform into 16-bit mono pcm"""
 		import numpy as np
 		return (0x7FFF * np.array(self.normalized)).astype(np.int16).tobytes()
+
+	def wav(self) -> None:
+		"""output as wav file"""
+		from mochaaudio import pcm_to_wav
+		pcm_to_wav(self.pcm, sample_rate=self.sample_rate)
+
+	@staticmethod
+	def square(freq: int = 440, t: float = 1, sample_rate: int = 44100):
+		# type: (int, float, int) -> Wave
+		return Wave(square(i/sample_rate, freq) for i in range(int(t*sample_rate)))
