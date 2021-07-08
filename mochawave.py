d97afd64e25491d9947bf8a82038945d91b4c2cf
mocha <3129210+Mocha2007@users.noreply.github.com>
2021-07-08 13:28:39 -0400

    fixed some bugs

--- mochawave.py
+++ mochawave.py
@@ -1,18 +1,23 @@
 from math import pi, log2, sin
-from typing import Callable, Iterable
+from typing import Callable, Iterable\
+
+def _test() -> None:
+	from mochaaudio import pcm_to_wav, play_file
+	pcm_to_wav(Wave.triangle().pcm)
+	play_file('output.wav')
 
 # basic waveforms
 def square(x: float, freq: float, amp: float = 1) -> float:
-	return amp*(2*(x/freq//1%2)-1)
+	return amp*(2*(x*freq//1%2)-1)
 
 def triangle(x: float, freq: float, amp: float = 1) -> float:
-	return amp*(abs(4*((x/freq)%1)-2)-1)
+	return amp*(abs(4*((x*freq)%1)-2)-1)
 
 def sine(x: float, freq: float, amp: float = 1) -> float:
-	return amp*sin(2*pi*x/freq)
+	return amp*sin(2*pi*x*freq)
 
 def sawtooth(x: float, freq: float, amp: float = 1) -> float:
-	return amp*(2*((x/freq)%1)-1)
+	return amp*(2*((x*freq)%1)-1)
 
 # note to freq conversions
 note_names = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
@@ -31,7 +36,7 @@ def n2f(n: str) -> float:
 # wave class for manipulating data
 class Wave:
 	def __init__(self, amplitude_data: Iterable[float], sample_rate: int = 44100) -> None:
-		self.amplitude_data = amplitude_data
+		self.amplitude_data = list(amplitude_data)
 		self.sample_rate = sample_rate
 
 	# properties
@@ -50,7 +55,7 @@ class Wave:
 	def pcm(self) -> bytes:
 		"""transform into 16-bit mono pcm"""
 		import numpy as np
-		return (0x7FFF * np.array(self.normalized)).astype(np.int16).tobytes()
+		return (0x7FFF * np.array(self.normalized.amplitude_data)).astype(np.int16).tobytes()
 
 	# double underscore methods
 
@@ -58,7 +63,7 @@ class Wave:
 		# type: (Wave, Wave) -> Wave
 		"""concatenate two waves"""
 		assert self.sample_rate == other.sample_rate
-		return Wave(list(self.amplitude_data) + list(other.amplitude_data), self.sample_rate)
+		return Wave(self.amplitude_data + other.amplitude_data, self.sample_rate)
 
 	def __len__(self) -> int:
 		"""sample points"""
@@ -97,7 +102,7 @@ class Wave:
 	@staticmethod
 	def from_function(f: Callable[[float, float, float], float], freq: int = 440, t: float = 1, sample_rate: int = 44100):
 		# type: (Callable[[float, float, float], float], int, float, int) -> Wave
-		return Wave(f(i/sample_rate, freq) for i in range(int(t*sample_rate)))
+		return Wave((f(i/sample_rate, freq) for i in range(int(t*sample_rate))), sample_rate)
 
 	@staticmethod
 	def sawtooth(freq: int = 440, t: float = 1, sample_rate: int = 44100):
