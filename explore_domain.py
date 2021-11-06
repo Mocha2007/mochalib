c5a6287de334c78cf469dfb5d3b8e447524d5fa8
mocha <3129210+Mocha2007@users.noreply.github.com>
2021-11-06 18:03:07 -0400

    download option OwO

--- explore_domain.py
+++ explore_domain.py
@@ -1,6 +1,5 @@
-from os import replace
-from re import findall, match, sub
-from typing import Dict, Set
+from re import findall, match, search, sub
+from typing import Dict, Set, Tuple
 from urllib.error import HTTPError
 from mochaweb import l as load
 
@@ -20,24 +19,31 @@ def getURLs(s: str) -> Set[str]:
 	# need the stupid g[0] nonsense because python things groups and matches are the same thing
 	return set(g[0] for g in findall(r'((?<=href=([\'"])).+?(?=\2))', s))
 
-def linksTo(url: str, domain: str) -> Set[str]:
-	return set(filter(lambda u: not match(r'https?://', u) or domain in u,
-		set(fixlink(uwu, domain) for uwu in getURLs(load(url).lower()))))
+def linksTo(url: str, domain: str) -> Tuple[str, Set[str]]:
+	source = load(url).lower()
+	return source, set(filter(lambda u: not match(r'https?://', u) or domain in u,
+		set(fixlink(uwu, domain) for uwu in getURLs(source))))
 
-def sitemap(start: str, domain: str, max_depth: int = 3, ignore: Set[str] = set()) -> Dict[str, Set[str]]:
+def sitemap(start: str, domain: str, max_depth: int = 3, ignore: Set[str] = set(), save: bool = False) -> Dict[str, Set[str]]:
 	print(f'loading (depth {max_depth}) {start} ...')
 	try:
-		urlmap = {start: linksTo(start, domain)}
+		source, links = linksTo(start, domain)
 	except (HTTPError, UnicodeEncodeError) as e: # 404 or invalid encoding
 		print(f'caught {e} @ {start}')
 		return {start: set()}
+	urlmap = {start: links}
+	# maybe save?
+	if save:
+		with open(f'saveddomains/{trimFilename(start)}', 'w+', encoding="utf-8") as file:
+			file.write(source)
+	# return early?
 	if max_depth == 0:
 		return urlmap
 	max_depth -= 1
 	for url in urlmap[start]:
 		if url in urlmap or url in ignore:
 			continue
-		for u, uu in sitemap(url, domain, max_depth, set(urlmap)|ignore).items():
+		for u, uu in sitemap(url, domain, max_depth, set(urlmap)|ignore, save).items():
 			if u in urlmap:
 				continue
 			urlmap[u] = uu
@@ -48,4 +54,9 @@ def test():
 	verd = 'http://www.zompist.com/virtuver.htm'
 	vs = load(verd).lower()
 	# return getURLs(vs)
-	return sitemap(verd, 'zompist.com')
+	return sitemap(verd, 'zompist.com', save=True)
+
+def trimFilename(s: str) -> str:
+	s = s[::-1]
+	s = search(r'[^#/]+(?=/)', s).group(0)
+	return s[::-1]
