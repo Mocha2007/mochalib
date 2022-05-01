52f97158a563da071c763427f61138687ac5ad35
mocha <3129210+Mocha2007@users.noreply.github.com>
2022-04-30 23:06:16 -0400

    finished mochamap2

--- mochamap2.py
+++ mochamap2.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from math import asin, atan2, cos, pi, sin
 from PIL import Image
 
@@ -17,8 +18,8 @@ class MapCoord:
 		self.x = x
 		self.y = y
 	def imagecoord(self, image: Image) -> ImageCoord:
-		x = (self.x + 1)/2 * image.width
-		y = (1 - self.y)/2 * image.height
+		x = int((self.x + 1)/2 * image.width)
+		y = int((1 - self.y)/2 * image.height)
 		return ImageCoord(x, y)
 
 class GeoCoord:
@@ -56,7 +57,35 @@ class Map:
 				for y in range(h):
 					coord_ = ImageCoord(x, y).geocoord_from_eq(output).project(projection).imagecoord(output)
 					x_, y_ = coord_.x, coord_.y
-					output.putpixel((x_, y_), im.getpixel((x, y)))
+					try:
+						output.putpixel((x_, y_), im.getpixel((x, y)))
+					except IndexError:
+						pass
 		output.save(f"maps/{destination_filename}", "PNG")
 
+# utility functions
+def newton_raphson(x: float, f, f_, max_iter = 100) -> float:
+	while ((x_ := x - f(x)/f_(x)) != x and 0 < max_iter):
+		x = x_
+		max_iter -= 1
+	return x
+# projections
+def mollweide(coord: GeoCoord) -> MapCoord:
+	lat, lon = coord.lat, coord.lon
+	if abs(lat) == pi/2:
+		theta = lat
+	else:
+		theta = newton_raphson(lat, 
+			lambda x: 2*x + sin(2*x) - pi*sin(lat),
+			lambda x: 2 + 2*cos(2*x))
+	x = lon * cos(theta) / pi
+	y = sin(theta)
+	# todo: remap to [-1, 1] for both
+	return MapCoord(x, y)
 # I need like... a map DATA object... ugh...
+# is this how it works??? I forgot tbh
+# (1) Take input image
+# (2) Turn image into matrix of data (lat/lon) -> color
+# (3) Turn use data matrix
+def test() -> None:
+	Map.from_eq('test.png', mollweide)
