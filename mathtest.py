d21c5b0db4c54e98f4068509b05d5f0fc8979ab1
mocha <3129210+Mocha2007@users.noreply.github.com>
2022-05-30 13:29:01 -0400

    settings...

--- mathtest.py
+++ mathtest.py
@@ -3,6 +3,11 @@ Math testing for the BOI
 """
 from random import randint
 
+settings = {
+	"a_must_be_positive": True,
+	"a_must_be_1": False,
+}
+
 def expect_yn(prompt: str = "") -> bool:
 	oo = {
 		'n': False,
@@ -14,23 +19,30 @@ def expect_yn(prompt: str = "") -> bool:
 		print("Invalid response.")
 	return oo[o]
 
-def random_factorable_quadratic(cmax: int = 9) -> tuple[tuple[int, int], tuple[int, int], tuple[int, int, int]]:
+def random_factorable_quadratic(cmax: int = 9,
+		a_must_be_positive: bool = settings["a_must_be_positive"],
+		a_must_be_1: bool = settings["a_must_be_1"]) \
+		-> tuple[tuple[int, int], tuple[int, int], tuple[int, int, int]]:
 	a1, a2, b1, b2 = [randint(-cmax, cmax) for _ in range(4)]
+	if a1*a2 == 0: # bad value
+		return random_factorable_quadratic(cmax, a_must_be_positive)
+	# normalize
+	if a_must_be_1:
+		a1 = a2 = 1
+	elif a_must_be_positive and a1 < 0 and a2 < 0:
+		a1, b1 = -a1, -b1
+		a2, b2 = -a2, -b2
+	elif a_must_be_positive and a1*a2 < 0:
+		if a1 < 0:
+			a1, b1 = -a1, -b1
+		else:
+			a2, b2 = -a2, -b2
 	return (a1, b1), (a2, b2), (a1*a2, a1*b2+a2*b1, b1*b2)
 
-def test_factoring(a_must_be_positive: bool = True) -> None:
+def test_factoring() -> None:
 	"""Factoring tester"""
 	while 1:
 		l1, l2, q = random_factorable_quadratic()
-		if l1[0] < 0 and l2[0] < 0:
-			l1 = tuple(-x for x in l1)
-			l2 = tuple(-x for x in l2)
-		if a_must_be_positive and q[0] < 0:
-			if l1[0] < 0:
-				l1 = tuple(-x for x in l1)
-			else:
-				l2 = tuple(-x for x in l2)
-			q = tuple(-x for x in q)
 		print(f"{q[0]}x² + {q[1]} x + {q[0]}")
 		input("Solve, and then press enter to see the answer:")
 		print(f"({l1[0]}x + {l1[1]})({l2[0]}x + {l2[1]})")
@@ -40,9 +52,35 @@ def test_factoring(a_must_be_positive: bool = True) -> None:
 		if expect_yn("Would you like to go back to the menu?\n\t"):
 			return
 
+def settings_menu() -> None:
+	while 1:
+		print("~~~~ Settings ~~~~")
+		# list settings
+		for i, j in settings.items():
+			print(f" {i} = {j}")
+		# try get key
+		while (option := input("Select option:\n\t").lower()) not in settings:
+			print(f"{option} is not a valid option; please try again:")
+		t = type(settings[option])
+		if t == bool: # python's bool thing doesn't work right with strings
+			t = lambda x: x.lower() in {"true", "t"}
+		# try get value
+		while 1:
+			try:
+				value = t(input("New Value: "))
+				break
+			except:
+				print("Invalid setting, please try again.")
+		settings[option] = value
+		print(f"{option} set to {value}")
+		if expect_yn("Would you like to go back to the menu?\n\t"):
+			return
+
 
 tests = {
 	"factoring": test_factoring,
+	"settings": settings_menu,
+	# quit
 	"quit": quit,
 }
 
