95e704c38222f4f4333e14dbd698a87db17b4f44
mocha <3129210+Mocha2007@users.noreply.github.com>
2023-03-21 13:08:00 -0400

    Update mochaastro2.py

--- mochaastro2.py
+++ mochaastro2.py
@@ -829,8 +829,8 @@ class Body:
 		# maybe try fitting the min() to arctan()???
 		gh = self.atmosphere.greenhouse
 		insolation = self.star.radiation_pressure_at(self.orbit.a)
-		C1 = 1.58745
-		C2 = 0.0652782
+		C1 = 1.58732
+		C2 = 0.0652868
 		# print(self.temp, gh/insolation)
 		return self.temp * C1 * (gh / insolation)**C2
 
