d9792fc662394687285a3cbeb37038a601cbd5ad
mocha <3129210+Mocha2007@users.noreply.github.com>
2024-02-28 13:22:16 -0500

    Update monopoly_stats.py

--- monopoly_stats.py
+++ monopoly_stats.py
@@ -6,10 +6,13 @@ def d6() -> int:
 def roll() -> int:
 	"""Accounts for doubles (up to three times)"""
 	s = 0
-	r = 0
-	while (d0 := d6()) == (d1 := d6()) and (r := r + 1) < 3:
+	for r in range(3):
+		d0, d1 = d6(), d6()
+		if d0 == d1 and r == 2: # speeding
+			break
 		s += d0 + d1
-	s += d0 + d1
+		if d0 != d1: # was this doubles?
+			break
 	return s
 
 def stats_roll(n = 1000000) -> None:
@@ -17,6 +20,6 @@ def stats_roll(n = 1000000) -> None:
 	from matplotlib.pyplot import show, stairs
 	# https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.hist.html
 	data = [roll() for _ in range(n)]
-	counts, bins = histogram(data, bins=range(37))
+	counts, bins = histogram(data, bins=range(36))
 	stairs(counts, bins)
 	show()
